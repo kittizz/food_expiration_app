a03056c76c9740ddeef9374fc48da072c39ba1c2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-07 10:08:27 +0000

    Updating to latest FlutterFlow output.

--- lib/authenticated/home/home_widget.dart
+++ lib/authenticated/home/home_widget.dart
@@ -33,16 +33,16 @@ class _HomeWidgetState extends State<HomeWidget> {
       _model.apiResulturm = await FoodexpirationGroup.getUserCall.call(
         deviceId: FFAppState().deviceId,
       );
-      if (!(_model.apiResulturm?.succeeded ?? true)) {
-        FFAppState().update(() {
-          FFAppState().deviceId = '';
-        });
-        GoRouter.of(context).prepareAuthEvent();
-        await authManager.signOut();
-        GoRouter.of(context).clearRedirectLocation();
-
+      if ((_model.apiResulturm?.succeeded ?? true)) {
         return;
       }
+
+      FFAppState().deviceId = '';
+      GoRouter.of(context).prepareAuthEvent();
+      await authManager.signOut();
+      GoRouter.of(context).clearRedirectLocation();
+
+      return;
 
       context.goNamedAuth('Welcome', context.mounted);
     });
