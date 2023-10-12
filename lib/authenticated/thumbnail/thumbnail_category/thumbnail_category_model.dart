3643fb3e48325882fe41ad96bb050a671c42ca4f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-12 14:55:19 +0000

    Updating to latest FlutterFlow output.

--- lib/authenticated/thumbnail/thumbnail_category/thumbnail_category_model.dart
+++ lib/authenticated/thumbnail/thumbnail_category/thumbnail_category_model.dart
@@ -1,6 +1,4 @@
 import '/backend/api_requests/api_calls.dart';
-import '/backend/backend.dart';
-import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -11,22 +9,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:octo_image/octo_image.dart';
 import 'package:provider/provider.dart';
 
 class ThumbnailCategoryModel extends FlutterFlowModel<ThumbnailCategoryWidget> {
-  ///  Local state fields for this page.
-
-  List<ThumbnailCategoryStruct> category = [];
-  void addToCategory(ThumbnailCategoryStruct item) => category.add(item);
-  void removeFromCategory(ThumbnailCategoryStruct item) =>
-      category.remove(item);
-  void removeAtIndexFromCategory(int index) => category.removeAt(index);
-  void insertAtIndexInCategory(int index, ThumbnailCategoryStruct item) =>
-      category.insert(index, item);
-  void updateCategoryAtIndex(
-          int index, Function(ThumbnailCategoryStruct) updateFn) =>
-      category[index] = updateFn(category[index]);
-
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
--- lib/authenticated/thumbnail/thumbnail_category/thumbnail_category_widget.dart
+++ lib/authenticated/thumbnail/thumbnail_category/thumbnail_category_widget.dart
@@ -1,6 +1,4 @@
 import '/backend/api_requests/api_calls.dart';
-import '/backend/backend.dart';
-import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -10,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:octo_image/octo_image.dart';
 import 'package:provider/provider.dart';
 import 'thumbnail_category_model.dart';
 export 'thumbnail_category_model.dart';
@@ -37,15 +36,6 @@ class _ThumbnailCategoryWidgetState extends State<ThumbnailCategoryWidget> {
       _model.apiCategory = await FoodexpirationGroup.thumbnailCategoryCall.call(
         deviceid: FFAppState().deviceId,
       );
-      if ((_model.apiCategory?.succeeded ?? true)) {
-        setState(() {
-          _model.category = functions
-              .toThumbnailCategoryStructList(
-                  (_model.apiCategory?.jsonBody ?? ''))
-              .toList()
-              .cast<ThumbnailCategoryStruct>();
-        });
-      }
     });
   }
 
@@ -107,79 +97,100 @@ class _ThumbnailCategoryWidgetState extends State<ThumbnailCategoryWidget> {
                 child: Padding(
                   padding:
                       EdgeInsetsDirectional.fromSTEB(12.0, 10.0, 12.0, 0.0),
-                  child: GridView(
-                    padding: EdgeInsets.zero,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 10.0,
-                      mainAxisSpacing: 10.0,
-                      childAspectRatio: 1.0,
-                    ),
-                    scrollDirection: Axis.vertical,
-                    children: [
-                      InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          context.pushNamed(
-                            'ThumbnailSelection',
-                            queryParameters: {
-                              'thumbnailCategoryId': serializeParam(
-                                1,
-                                ParamType.int,
+                  child: Builder(
+                    builder: (context) {
+                      final list = functions
+                          .toThumbnailCategoryStructList(
+                              (_model.apiCategory?.jsonBody ?? ''))
+                          .toList();
+                      return GridView.builder(
+                        padding: EdgeInsets.zero,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 10.0,
+                          mainAxisSpacing: 10.0,
+                          childAspectRatio: 1.0,
+                        ),
+                        scrollDirection: Axis.vertical,
+                        itemCount: list.length,
+                        itemBuilder: (context, listIndex) {
+                          final listItem = list[listIndex];
+                          return InkWell(
+                            splashColor: Colors.transparent,
+                            focusColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            onTap: () async {
+                              context.pushNamed(
+                                'ThumbnailSelection',
+                                queryParameters: {
+                                  'thumbnailCategoryId': serializeParam(
+                                    1,
+                                    ParamType.int,
+                                  ),
+                                }.withoutNulls,
+                              );
+                            },
+                            child: Card(
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              color: FlutterFlowTheme.of(context)
+                                  .secondaryBackground,
+                              elevation: 4.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
                               ),
-                            }.withoutNulls,
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    child: OctoImage(
+                                      placeholderBuilder:
+                                          OctoPlaceholder.blurHash(
+                                        listItem.image.blurHash,
+                                      ),
+                                      image: NetworkImage(
+                                        valueOrDefault<String>(
+                                          functions
+                                              .getImage(listItem.image.path),
+                                          'https://images.unsplash.com/photo-1484503793037-5c9644d6a80a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw4fHx3aGl0ZXxlbnwwfHx8fDE2OTE4OTAzNTN8MA&ixlib=rb-4.0.3&q=80&w=1080',
+                                        ),
+                                      ),
+                                      width: 500.0,
+                                      height: 500.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: double.infinity,
+                                    height: double.infinity,
+                                    decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                        colors: [
+                                          Color(0x00FFFFFF),
+                                          Color(0x93000000)
+                                        ],
+                                        stops: [0.5, 1.0],
+                                        begin: AlignmentDirectional(0.0, -1.0),
+                                        end: AlignmentDirectional(0, 1.0),
+                                      ),
+                                    ),
+                                    child: Align(
+                                      alignment:
+                                          AlignmentDirectional(0.00, 1.00),
+                                      child: Text(
+                                        'ทะเล',
+                                        style: FlutterFlowTheme.of(context)
+                                            .titleMedium,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           );
                         },
-                        child: Card(
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          color:
-                              FlutterFlowTheme.of(context).secondaryBackground,
-                          elevation: 4.0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          child: Stack(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(8.0),
-                                child: Image.network(
-                                  'https://images.unsplash.com/photo-1484503793037-5c9644d6a80a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHw4fHx3aGl0ZXxlbnwwfHx8fDE2OTE4OTAzNTN8MA&ixlib=rb-4.0.3&q=80&w=1080',
-                                  width: 500.0,
-                                  height: 500.0,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Container(
-                                width: double.infinity,
-                                height: double.infinity,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    colors: [
-                                      Color(0x00FFFFFF),
-                                      Color(0x93000000)
-                                    ],
-                                    stops: [0.5, 1.0],
-                                    begin: AlignmentDirectional(0.0, -1.0),
-                                    end: AlignmentDirectional(0, 1.0),
-                                  ),
-                                ),
-                                child: Align(
-                                  alignment: AlignmentDirectional(0.00, 1.00),
-                                  child: Text(
-                                    'ทะเล',
-                                    style: FlutterFlowTheme.of(context)
-                                        .titleMedium,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                      );
+                    },
                   ),
                 ),
               ),
