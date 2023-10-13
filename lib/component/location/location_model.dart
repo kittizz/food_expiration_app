3aac18d17de188c004cac20b0bd5c52cf08fd316
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-13 12:31:56 +0000

    Updating to latest FlutterFlow output.

--- lib/component/location/location_model.dart
+++ lib/component/location/location_model.dart
@@ -1,6 +1,5 @@
 import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
-import '/flutter_flow/flutter_flow_expanded_image_view.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
@@ -12,7 +11,6 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:octo_image/octo_image.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
 class LocationModel extends FlutterFlowModel<LocationWidget> {
