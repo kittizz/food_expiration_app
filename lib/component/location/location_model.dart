e7d34da9db9c36d9a6d2645fcf11cbc9c6b86fbd
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-13 11:58:07 +0000

    Updating to latest FlutterFlow output.

--- lib/component/location/location_model.dart
+++ lib/component/location/location_model.dart
@@ -1,12 +1,18 @@
+import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_expanded_image_view.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'location_widget.dart' show LocationWidget;
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:octo_image/octo_image.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
 class LocationModel extends FlutterFlowModel<LocationWidget> {
--- lib/component/location/location_widget.dart
+++ lib/component/location/location_widget.dart
@@ -1,11 +1,17 @@
+import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_expanded_image_view.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:octo_image/octo_image.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 import 'location_model.dart';
 export 'location_model.dart';
@@ -19,7 +25,7 @@ class LocationWidget extends StatefulWidget {
   }) : super(key: key);
 
   final String? name;
-  final String? image;
+  final ImageStruct? image;
   final int? count;
 
   @override
@@ -126,61 +132,120 @@ class _LocationWidgetState extends State<LocationWidget>
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(5.0, 0.0, 0.0, 0.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 5.0, 0.0, 0.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.name!,
-                            style: FlutterFlowTheme.of(context)
-                                .headlineMedium
-                                .override(
-                                  fontFamily: FlutterFlowTheme.of(context)
-                                      .headlineMediumFamily,
-                                  color:
-                                      FlutterFlowTheme.of(context).primaryText,
-                                  fontSize: 16.0,
-                                  useGoogleFonts: GoogleFonts.asMap()
-                                      .containsKey(FlutterFlowTheme.of(context)
-                                          .headlineMediumFamily),
-                                ),
-                          ),
-                          Text(
-                            'มีของทั้งหมด ${valueOrDefault<String>(
-                              formatNumber(
-                                widget.count,
-                                formatType: FormatType.decimal,
-                                decimalType: DecimalType.periodDecimal,
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0.0, 5.0, 0.0, 0.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.name!,
+                          style: FlutterFlowTheme.of(context)
+                              .headlineMedium
+                              .override(
+                                fontFamily: FlutterFlowTheme.of(context)
+                                    .headlineMediumFamily,
+                                color: FlutterFlowTheme.of(context).primaryText,
+                                fontSize: 16.0,
+                                useGoogleFonts: GoogleFonts.asMap().containsKey(
+                                    FlutterFlowTheme.of(context)
+                                        .headlineMediumFamily),
                               ),
-                              '0',
-                            )} ชิ้น',
-                            style: FlutterFlowTheme.of(context).bodyMedium,
+                        ),
+                        Text(
+                          'มีของทั้งหมด ${valueOrDefault<String>(
+                            formatNumber(
+                              widget.count,
+                              formatType: FormatType.decimal,
+                              decimalType: DecimalType.periodDecimal,
+                            ),
+                            '0',
+                          )} ชิ้น',
+                          style: FlutterFlowTheme.of(context).bodyMedium,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(),
+                child: InkWell(
+                  splashColor: Colors.transparent,
+                  focusColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onTap: () async {
+                    await Navigator.push(
+                      context,
+                      PageTransition(
+                        type: PageTransitionType.fade,
+                        child: FlutterFlowExpandedImageView(
+                          image: OctoImage(
+                            placeholderBuilder: OctoPlaceholder.blurHash(
+                              widget.image!.blurHash,
+                            ),
+                            image: CachedNetworkImageProvider(
+                              valueOrDefault<String>(
+                                functions.getImage(widget.image!.path),
+                                'https://th-bkk-1.xvercloud.com/food-expiration/images/lay.webp',
+                              ),
+                            ),
+                            fit: BoxFit.contain,
+                            errorBuilder: (context, error, stackTrace) =>
+                                Image.asset(
+                              'assets/images/error_image.png',
+                              fit: BoxFit.contain,
+                            ),
                           ),
-                        ],
+                          allowRotation: false,
+                          tag: valueOrDefault<String>(
+                            functions.getImage(widget.image!.path),
+                            'https://th-bkk-1.xvercloud.com/food-expiration/images/lay.webp',
+                          ),
+                          useHeroAnimation: true,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Hero(
+                    tag: valueOrDefault<String>(
+                      functions.getImage(widget.image!.path),
+                      'https://th-bkk-1.xvercloud.com/food-expiration/images/lay.webp',
+                    ),
+                    transitionOnUserGestures: true,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: OctoImage(
+                        placeholderBuilder: OctoPlaceholder.blurHash(
+                          widget.image!.blurHash,
+                        ),
+                        image: CachedNetworkImageProvider(
+                          valueOrDefault<String>(
+                            functions.getImage(widget.image!.path),
+                            'https://th-bkk-1.xvercloud.com/food-expiration/images/lay.webp',
+                          ),
+                        ),
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) =>
+                            Image.asset(
+                          'assets/images/error_image.png',
+                          width: double.infinity,
+                          height: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ],
-                ),
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.network(
-                  valueOrDefault<String>(
-                    widget.image,
-                    'https://th-bkk-1.xvercloud.com/food-expiration/images/lay.webp',
                   ),
-                  height: 85.0,
-                  fit: BoxFit.cover,
                 ),
               ),
             ],
