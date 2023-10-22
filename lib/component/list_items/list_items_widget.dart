7bde0dad4668b2cc5bf38d30dbecd1349c909504
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-22 07:46:43 +0000

    Updating to latest FlutterFlow output.

--- lib/component/list_items/list_items_widget.dart
+++ lib/component/list_items/list_items_widget.dart
@@ -264,6 +264,12 @@ class _ListItemsWidgetState extends State<ListItemsWidget>
                       ),
                       name: itemsLocalItem.name,
                       image: functions.getImage(itemsLocalItem.image.path),
+                      location: FFAppState()
+                          .locations
+                          .where((e) => e.id == widget.locationId)
+                          .toList()
+                          .first
+                          .name,
                       preDay: itemsLocalItem.forewarnDay,
                       imageBlurhash: itemsLocalItem.image.blurHash,
                       id: itemsLocalItem.id,
