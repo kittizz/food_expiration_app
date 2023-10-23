93aca6f0db319a287ac07f3573a7e21471b9484f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-23 15:50:20 +0000

    Updating to latest FlutterFlow output.

--- lib/authenticated/item_info/item_info_widget.dart
+++ lib/authenticated/item_info/item_info_widget.dart
@@ -450,8 +450,8 @@ class _ItemInfoWidgetState extends State<ItemInfoWidget> {
                                   final selectedMedia =
                                       await selectMediaWithSourceBottomSheet(
                                     context: context,
-                                    maxWidth: 512.00,
-                                    maxHeight: 512.00,
+                                    maxWidth: 1024.00,
+                                    maxHeight: 1024.00,
                                     imageQuality: 100,
                                     allowPhoto: true,
                                     includeBlurHash: true,
